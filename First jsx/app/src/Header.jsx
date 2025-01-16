eae6131c4ae0cedfea3290a016a49bae33766148
jalal hemmo <jalal.i.hemmo@gmail.com>
2025-01-16 11:07:29 +0200

    first JSX

---
+++ First jsx/app/src/Header.jsx
@@ -0,0 +1,23 @@
+
+
+function Header(){
+
+    return(
+        <>
+            <div className='header'>
+
+                <h1>Hello Dojo !</h1>
+                <h3>Things I need to do</h3>
+                <ul>
+                    <li>Learn React</li>
+                    <li>Climn Mt. Everest</li>
+                    <li>Run a marathin</li>
+                    <li>Feed the Dogs</li>
+                </ul>
+            </div>
+        </>
+    )
+}
+
+
+export default Header;
